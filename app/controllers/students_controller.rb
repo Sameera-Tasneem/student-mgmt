acbf22a6d80dd606bfdf077c19393c3aaa50e9e2
Sameera-Tasneem <60311547+Sameera-Tasneem@users.noreply.github.com>
2023-01-11 20:42:46 +0530

    form for resource and create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,26 @@
 class StudentsController < ApplicationController
     
-end
+    def index
+        @students = Student.all
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+            redirect_to students_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name,:last_name,:email,:phone_number)
+    end
+ end
+# '@' symbol is used in controller variable if you want to display anything on browser.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+student = Student.create(
+    first_name: 'Sam',
+    last_name: 'Roy',
+    email: 'sam@gm.com',
+    phone_number: '9283848272'
+)
+
+#can also write as 
+# student = Student.new(
+#     first_name: 'Sam',
+#     last_name: 'Roy',
+#     email: 'sam@gm.com',
+#     phone_number: '9283848272'
+# )
+# student.save
+
+20.times do |i|
+    puts "Creating student #{i+1}"
+    Student.create(
+        first_name: "Student #{i+1}",
+        last_name: "Lname #{i+1}",
+        email: "student#{i+1}@bootcamp.com"
+    )
+end
