6224abfd96650dc0c76a7c80a430f755e0028ff5
Sameera-Tasneem <60311547+Sameera-Tasneem@users.noreply.github.com>
2023-01-18 16:45:18 +0530

    partials and notice flash

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
     def create
         @student = Student.new(student_params)
         if @student.save
-            redirect_to students_path
+            redirect_to students_path, notice: 'Student has been created successfully'
         else
             render :new
         end
@@ -28,7 +28,7 @@ class StudentsController < ApplicationController
     def update
         @student = Student.find(params[:id])
         if @student.update(student_params)
-            redirect_to student_path
+            redirect_to student_path, notice: 'Student has been updated successfully'
         else
             render :edit
         end
@@ -37,11 +37,8 @@ class StudentsController < ApplicationController
     def destroy
         @student = Student.find(params[:id])
         @student.destroy
-        redirect_to students_path
+        redirect_to students_path, notice: 'Student has been deleted successfully'
     end
-
-
-
 
     private
 
